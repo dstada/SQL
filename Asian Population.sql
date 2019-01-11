7282dea7160b9df15aa8a94a2ef8e724e8872b12
Dick Stada <dickstada@gmail.com>
2019-01-11 14:26:17 +0100

    Add files via upload

---
+++ Asian Population.sql
@@ -0,0 +1,10 @@
+SELECT SUM(CITY.Population)
+FROM CITY
+INNER JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
+WHERE COUNTRY.Continent = 'Asia';
+
+/*
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
